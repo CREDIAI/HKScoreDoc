a401cf9198e19c9a067e68a9f83d81a77f249af7
Derek <45706662+derek-wkwk@users.noreply.github.com>
2020-07-24 10:16:17 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ source_parsers = {
 }
  
 source_suffix = ['.rst', '.md']
-
+master_doc = 'index'
 # -- Project information -----------------------------------------------------
 
 project = 'hkscore'
